f92bff7cdf0f105d287beee777a968b7c4a8a0c3
Takshin Varma <takshin.varma@gmail.com>
2026-04-20 21:30:40 +0530

    adding failure for testing cicd

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -16,3 +16,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(1, -1), 0)
         self.assertEqual(add(1, 0), 1)
         self.assertEqual(add(-1, 0), -1)
+    
+    # test case that fails for the add function
+    def test_add_failure(self):
+        self.assertEqual(add(1, 2), 4)
